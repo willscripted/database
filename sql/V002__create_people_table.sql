2e0c8f407b63062fe01c15e0b71885359a9297f2
Will O'Brien <1069495+willscripted@users.noreply.github.com>
2024-06-24 16:03:37 -0400

    Fix broken tests

--- sql/V002__create_people_table.sql
+++ sql/V002__create_people_table.sql
@@ -1,3 +1,10 @@
+CREATE TABLE short_ids_for_people (
+  id VARCHAR(3) NOT NULL UNIQUE,
+  people_id UUID NULL,
+
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE people (
   id          UUID        NOT NULL  DEFAULT uuid_generate_v4(),
   short_id    VARCHAR(3)  NOT NULL  REFERENCES short_ids_for_people(id),
@@ -7,13 +14,6 @@ CREATE TABLE people (
   metadata    JSON        NOT NULL  DEFAULT '{}'::JSON,
 
   created_at  TIMESTAMPTZ NOT NULL  DEFAULT now(),
-
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE short_ids_for_people (
-  id VARCHAR(3) NOT NULL UNIQUE,
-  people_id UUID NULL,
 
   PRIMARY KEY (id)
 );
